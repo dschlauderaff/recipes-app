9f3dae90e5e3f9338b54ab4101afa8882e121a79
Dave Schlauderaff <dschlaud@gmail.com>
2019-10-30 15:06:46 -0500

    add pages controller, model, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+
+  get 'home' => 'pages#home'
+  get 'profile' => 'pages#profile'
+  get 'explore' => 'pages#explore'
   resources :users
 
-  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
